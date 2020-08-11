4d41006053207fcd11f370845936da10339d114c
vlader <Vladarscb@gmail.com>
2020-08-11 15:58:23 +1000

    3.4. Заголовок в файле java [#330689]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,7 +1,9 @@
 package ru.job4j;
 
 public class Main {
-    public static void main(String [] args) {
-        System.out.println("Hello world");
+
+        public static void main(String[] args) {
+            System.out.println("19.01.2020");
+        }
     }
-}
+
