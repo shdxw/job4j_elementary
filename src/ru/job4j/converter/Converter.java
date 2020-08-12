e1259ace330cad75f265e46513840ea31cc5f9c4
vlader <Vladarscb@gmail.com>
2020-08-12 11:59:06 +1000

    5. Конвертер валюты [#330667]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -12,11 +12,5 @@ public class Converter {
             return rsl;
         }
 
-    public static void main(String[] args) {
-        int euro = Converter.rubleToEuro(140);
-        int dolrs = Converter.rubleToDollar(900);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("900 rubles are " + dolrs + " dollars.");
-    }
     }
 
