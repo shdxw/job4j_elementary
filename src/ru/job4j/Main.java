6993eb15f1e3e6fe08819c680623887d6963ae03
vlader <Vladarscb@gmail.com>
2020-08-13 13:41:12 +1000

    0. Что такое тестирование [#330706]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,14 +1,20 @@
 package ru.job4j;
+import ru.job4j.converter.Converter;
 
 public class Main {
 
     public static void main(String[] args) {
-        String idea = "i like Java!";
-        System.out.println(idea);
-        idea = idea + "But im a newbie";
-        int year = 2020;
-        idea += 2020;
-        System.out.println(idea);
+        int inE = 210;
+        int inD = 120;
+        int expE = 3;
+        int expD = 2;
+        int outE = Converter.rubleToEuro(inE);
+        int outD = Converter.rubleToDollar(inD);
+        boolean passE = expE == outE;
+        boolean passD = expD == outD;
+        System.out.println("210 rubles are 3 euro. Result: " + passE);
+        System.out.println("120 rubles are 2 dollars. Result: " + passD);
     }
+
     }
 
