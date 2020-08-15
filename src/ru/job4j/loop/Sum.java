d0ba0fb828a828b6c77039c7fd0b26bd82c5db6b
vlader <Vladarscb@gmail.com>
2020-08-15 11:52:57 +1000

    5.0.1. Сумма чисел. [#330727]

---
+++ src/ru/job4j/loop/Sum.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Sum {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+           sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 2));
+        System.out.println(sum(2, 10));
+    }
+}
