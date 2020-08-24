e32639585a1b2987a8729f7eb26469b144cd4267
vlader <Vladarscb@gmail.com>
2020-08-24 11:11:19 +1000

    1. Чтение кода и базовые ошибки [#330822]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -1,11 +1,16 @@
 package ru.job4j.calculator;
 
 public class Fit {
+    /**
+     * Метод  для подсчета идеального веса мужчин
+     */
     public static double manWeight(short height) {
         double rsl = (height - 100) * 1.15;
         return rsl;
     }
-
+    /**
+     * Метод  для подсчета идеального веса женщин
+     */
     public static double womanWeight(short height) {
         double rsl = (height - 110) * 1.15;
         return rsl;
